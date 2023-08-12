416fe1d4c2d7236edc7e11b8e6859d93717fb86b
Fatma-Alhabbash <124456674+Fatma-Alhabbash@users.noreply.github.com>
2023-08-12 14:09:18 +0300

    Update About-Me.py

--- pages/About-Me.py
+++ pages/About-Me.py
@@ -20,6 +20,5 @@ def about_me():
     st.markdown("I look forward to expanding my knowledge in Data Science and AI, staying updated with the latest advancements in the field. I am excited about contributing to real-world projects and leveraging data to create positive impacts on society.")
 
     st.markdown("## Github link")
-    <a href="https://github.com/Fatma-Alhabbash"><img src="/github-logo.png"></a>
-
+    st.markdown("[![GitHub Logo](github-logo.png)](https://github.com/Fatma-Alhabbash)")
 about_me()
